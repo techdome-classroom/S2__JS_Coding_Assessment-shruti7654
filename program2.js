742417c64a4ce1532e59ed20d93479a1d90f5657
shruti7654 <sbop010801@gmail.com>
2024-09-25 20:20:58 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,6 +3,22 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    class Solution {
+        romanToInt(s) {
+            const romanMap = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+            let total = 0;
+    
+            for (let i = 0; i < s.length; i++) {
+                if (i + 1 < s.length && romanMap[s[i]] < romanMap[s[i + 1]]) {
+                    total -= romanMap[s[i]];
+                } else {
+                    total += romanMap[s[i]];
+                }
+            }
+    
+            return total;
+        }
+    }
     
 };
 
