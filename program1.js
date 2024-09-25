170965b3362451d57ccb0bac3b08e88ae38d614d
shruti7654 <sbop010801@gmail.com>
2024-09-25 20:15:38 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -9,14 +9,14 @@ var isValid = function(s) {
             for (let char of s) {
                 if (brackets[char]) { // if we have a opening bracket, append it to our stack
                     stack.push(char);
-                } else if (stack.length && brackets[stack[stack.length - 1]] === char) { // if we have a closing bracket, check if it matches the top of the stack
+                } else if (stack.length && brackets[stack[stack.length - 1]] === char) { 
                     stack.pop();
                 } else {
-                    return false; // if the brackets don't match, return False
+                    return false; 
                 }
             }
     
-            return !stack.length; // if the stack is empty, it means all brackets are properly terminated
+            return !stack.length; 
         };
     
 
