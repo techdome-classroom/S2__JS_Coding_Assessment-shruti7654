c54d086b5855e5a58590bf8a9a69de1ccc046815
shruti7654 <sbop010801@gmail.com>
2024-09-25 20:15:58 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,11 +3,11 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-            const stack = []; // initializing our stack
-            const brackets = { '(': ')', '{': '}', '[': ']' }; // a hash-map to quickly compare opening and closing brackets
+            const stack = []; 
+            const brackets = { '(': ')', '{': '}', '[': ']' }; 
     
             for (let char of s) {
-                if (brackets[char]) { // if we have a opening bracket, append it to our stack
+                if (brackets[char]) { 
                     stack.push(char);
                 } else if (stack.length && brackets[stack[stack.length - 1]] === char) { 
                     stack.pop();
